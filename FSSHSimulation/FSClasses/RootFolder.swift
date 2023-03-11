748077729d01e46a9f8f4f65f88fa1e12c8142d2
Jarek Šedý <jareksedy@icloud.com>
2023-03-11 11:30:04 +0600

    Added RootFolder class.

---
+++ FSSHSimulation/FSClasses/RootFolder.swift
@@ -0,0 +1,12 @@
+//
+//  RootFolder.swift
+//  FSSHSimulation
+//
+//  Created by Ярослав on 11.03.2023.
+//
+
+class RootFolder: Folder {
+    init(files: [AbstractFile]) {
+        super.init(name: "", files: files)
+    }
+}
