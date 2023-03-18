d085368dc4123c32149e2533485c281343c6c1f8
Jarek Šedý <jareksedy@icloud.com>
2023-03-18 12:42:58 +0600

    Adding more tests.

--- FSSHSimulationTests/FSSHSimulationTests.swift
+++ FSSHSimulationTests/FSSHSimulationTests.swift
@@ -29,4 +29,42 @@ final class FSSHSimulationTests: XCTestCase {
         chdirCommand.main(arguments: ["~"])
         XCTAssert(environment.currentDirectory.path == "/home/root")
     }
+    
+    func testMkdirChdir() throws {
+        let mkdirCommand = Mkdir()
+        let chdirCommand = Chdir()
+        
+        chdirCommand.main(arguments: ["~"])
+        XCTAssert(environment.currentDirectory.path == "/home/root")
+        
+        for i in 0 ... 9_999 {
+            mkdirCommand.main(arguments: ["test\(i)"])
+            chdirCommand.main(arguments: ["test\(i)"])
+            XCTAssert(environment.currentDirectory.name == "test\(i)")
+        }
+        
+        chdirCommand.main(arguments: ["~"])
+        XCTAssert(environment.currentDirectory.path == "/home/root")
+        
+        mkdirCommand.main(arguments: ["/home/root/test0/test1/test2/test3/test4/test5/test6/test7/test8/hello"])
+        chdirCommand.main(arguments: ["/home/root/test0/test1/test2/test3/test4/test5/test6/test7/test8/hello"])
+        XCTAssert(environment.currentDirectory.name == "hello")
+        
+        chdirCommand.main(arguments: ["~"])
+        XCTAssert(environment.currentDirectory.path == "/home/root")
+        
+        mkdirCommand.main(arguments: ["/usr"])
+        mkdirCommand.main(arguments: ["../../usr/bin"])
+        chdirCommand.main(arguments: ["/usr/bin"])
+        XCTAssert(environment.currentDirectory.name == "bin")
+        
+        chdirCommand.main(arguments: ["/"])
+        XCTAssert(environment.currentDirectory.path == "/")
+        
+        for i in 0 ... 9_999 {
+            mkdirCommand.main(arguments: ["temp_directory\(i)"])
+        }
+        
+        XCTAssert(environment.rootDirectory.nodes.count == 10_002)
+    }
 }
