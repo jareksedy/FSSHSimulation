190e04a2379aaa7645ca6232ceb3a9b1cbc287ba
Jarek Šedý <jareksedy@icloud.com>
2023-03-21 22:17:34 +0600

    Remove unused code.

--- FSSHSimulation/Shell/Classes/Test.swift
+++ FSSHSimulation/Shell/Classes/Test.swift
@@ -7,18 +7,6 @@
 
 final class Test: CommandProtocol {
     func main(arguments: [String]) throws {
-        guard !arguments.isEmpty else {
-            throw Errors.usageDirectoryName
-        }
-        
-        guard arguments.count == 1 else {
-            throw Errors.tooManyArguments
-        }
-        
-        let argument = arguments.first
-        
-        //print("TOKN :", argument?.tokenize())
-        print("NAME :", argument!.stripPath())
-        print("PATH :", argument!.stripFilename())
+        throw Errors.tooManyArguments
     }
 }
